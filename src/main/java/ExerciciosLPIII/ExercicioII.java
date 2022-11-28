3e09fbcb990f568d3fe0c2b2ea006762fc229cd1
Felipe <felipeshonorato@gmail.com>
2022-11-28 18:49:33 -0300

    Resolução do exercício II.

---
+++ src/main/java/ExerciciosLPIII/ExercicioII.java
@@ -0,0 +1,26 @@
+package ExerciciosLPIII;
+
+import java.util.Scanner;
+
+public class ExercicioII {
+    public static void main(String[] args) {
+        // Exercício I proposto no treinamento de Lógica de Progamação III com Java pela Capgemini.
+
+        Scanner scanner = new Scanner(System.in);
+        double numeroA = 0;
+        double numeroB = 0;
+
+        System.out.print("Digite o valor de A:");
+
+        numeroA = scanner.nextDouble();
+
+        System.out.print("Digite o valor de B:");
+
+        numeroB = scanner.nextDouble();
+
+        System.out.println("A soma resultante entre o número A e B é: " + (numeroA + numeroB));
+        System.out.println("A subtração resultante entre o número A e B é: " + (numeroA - numeroB));
+        System.out.println("A multiplicação resultante entre o número A e B é: " + (numeroA * numeroB));
+        System.out.println("A divisão resultante entre o número A e B é: " + (numeroA / numeroB));
+    }
+}
